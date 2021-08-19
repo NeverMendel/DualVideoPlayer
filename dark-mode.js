94284d013cf6b8e5273876b74ac8ecb0d8dc6911
Davide Cazzin <cazzindavide@gmail.com>
2021-08-19 10:05:26 +0200

    Save dark-mode flag in cookies instead of local storage

--- dark-mode.js
+++ dark-mode.js
@@ -4,9 +4,9 @@ let darkModeEnabled = false;
 let darkModeButton = document.getElementById("dark_mode_button");
 darkModeButton.addEventListener("click", switchTheme);
 
-console.log("Dark mode enabled: " + localStorage.getItem("dark-mode"));
+console.log("Dark mode enabled: " + getCookie("dark-mode"));
 
-if (localStorage.getItem("dark-mode") === "true") {
+if (getCookie("dark-mode") === "true") {
     switchTheme();
 }
 
@@ -19,5 +19,28 @@ function switchTheme() {
         darkModeButton.innerHTML = "Dark Mode";
     }
     darkModeEnabled = !darkModeEnabled;
-    localStorage.setItem("dark-mode", darkModeEnabled);
+    setCookie("dark-mode", darkModeEnabled, 10 * 365);
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
